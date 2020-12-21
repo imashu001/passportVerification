d98e3f6e8748b88fc5f2b57a60f773bbf36b2fc7
imashu001 <imashu001@gmail.com>
2020-12-21 16:17:13 +0530

    InitialCommit

---
+++ routes/users.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+const passport = require("passport");
+const usersController = require("../controllers/users_controller");
+
+router.get("/profile", passport.checkAuthentication, usersController.profile);
+
+router.get("/sign-up", usersController.signUp);
+router.get("/sign-in", usersController.signIn);
+
+router.post("/create", usersController.create);
+
+router.get("/sign-out", usersController.destroySession);
+
+// use passport authenticate
+router.post(
+  "/create-session",
+  passport.authenticate("local", { failureRedirect: "/users/sign-in" }),
+  usersController.createSession
+);
+
+module.exports = router;
